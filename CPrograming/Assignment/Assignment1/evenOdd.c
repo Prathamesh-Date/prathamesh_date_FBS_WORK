b1d36b2f3bb6464e0d29dd2ef3104f8d8b4b8d86
Prathamesh-Date <prathameshdate3451@gmail.com>
2025-07-23 20:53:44 +0530

    Update evenOdd.c

--- CPrograming/Assignment/Assignment1/evenOdd.c
+++ CPrograming/Assignment/Assignment1/evenOdd.c
@@ -1,25 +1,15 @@
 #include<stdio.h>
-int isprime(int no);
-void main(){
-	int no;	
-	printf("Enter number :");
-	scanf("%d",&no);
+int main()
+{
+	int a;
+	printf("enter the value :");
+	scanf("%d",&a);
 	
-	int res =evenodd(no);
-	
-	if (res==0){
-		printf("Number is even");
+	if(a%2!=0) {
+		printf("value is even \n",a);	
+	} else{
+		printf("value is odd\n",a);
 	}
-	else{
-		printf("Number is odd");
-	}
-}
-int evenodd( int no){
-	
-	if(no%2==0){
-		return 0;
-	}
-	return 1;
-	
+	return 0;
 }
 
